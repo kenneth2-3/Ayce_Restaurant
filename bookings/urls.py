07a78ed5bba50aed8820450b0bd5fb4be649b006
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-24 19:17:07 +0100

    Add url paths for the templates created

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('menu/', views.menu, name='menu'),
+    path('book/', views.book_table, name='book_table'),
+    path('confirmation/', views.booking_confirmation, name='booking_confirmation'),
 ]
