64658db31a19f4e17dae4cf3dab837d46f1fbaec
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-24 17:06:13 +0100

    Modify key settings

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ AUTHENTICATION_BACKENDS = (
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
